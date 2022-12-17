cb725ac76a500b803ea7acbd20821449140cb9a6
arwebit <arwebcs@gmail.com>
2022-12-17 18:30:00 +0530

    Webadmin complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CategoryService } from '../common/rest-service/category.service';
+import { PostService } from '../common/rest-service/post.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  pageName: string = "Dashboard";
+  categoryRecord: number = 0;
+  blogRecord: number = 0;
+  statusCode: number = 0;
 
+  constructor(private categoryService: CategoryService, private postService: PostService) {
+    this.categoryService.listCategory().subscribe((result) => {
+      this.statusCode = result.statusCode;
+      if (this.statusCode == 200) {
+        this.categoryRecord = result.records;
+      }
+    }), (err: HttpErrorResponse): void => {
+      alert(err);
+    };
+
+    this.postService.listPost().subscribe((result) => {
+      this.statusCode = result.statusCode;
+      if (this.statusCode == 200) {
+        this.blogRecord = result.records;
+      }
+    }), (err: HttpErrorResponse): void => {
+      alert(err);
+    };
+  }
+
+  ngOnInit(): void { }
 }
